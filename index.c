8e83fcea6ebe97dfb15ccff29c64b6a974d46de3
Rajyalaxmi Vanam <rajilucky222@gmail.com>
2021-09-03 16:27:06 +0530

    Update index.c

--- index.c
+++ index.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include <stdlib.h>
 
 void displayPathtoPrincess(int n, char **grid){
-    //logic here
-    // int princess = -1;
     int m_pos = (n-1)/2;
     char *dir1, *dir2;
     
     if(grid[0][0]=='P' || grid[0][0]=='p'){
-        // princess = 0;
         dir1 = "LEFT";
         dir2 = "UP";
     }
     else if(grid[0][n-1]=='P' || grid[0][n-1]=='p'){
-        // princess = 1;     
         dir1 = "RIGHT";
         dir2 = "UP";
     }
     else if(grid[n-1][0]=='P' || grid[n-1][0]=='p'){
-        // princess = 2;
         dir1 = "DOWN";
         dir2 = "LEFT";
     }
     else{
-        // princess = 3;
         dir1 = "DOWN";
         dir2 = "RIGHT";
     }
@@ -35,17 +28,13 @@ void displayPathtoPrincess(int n, char **grid){
 }
 
 int main(void) {
-
     int m;
     scanf("%d", &m);
     char **grid = (char **)malloc(m*sizeof(char *));
-    // char grid[m][m]={};
-    char line[m];
 
     for(int i=0; i<m; i++) {
-        scanf("%s", line);
         grid[i] = (char *)malloc(m*sizeof(char));
-        strcpy(grid[i], line);
+        scanf("%s", grid[i]);
     }
     displayPathtoPrincess(m,grid);
     return 0;
